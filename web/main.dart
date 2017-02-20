df5d5ef80b0500fce25689d99ce75f43decbe890
ice-ice <kwge107@gmail.com>
2017-02-20 23:59:44 +0000

    Add a tiny game for login
    should be pretty easy

--- web/main.dart
+++ web/main.dart
@@ -7,36 +7,71 @@ import 'lib/mud.dart';
 int x = 0, y = 0;
 
 void main() {
-  querySelector('#output').text = 'Welcome to Multi-User Dungeon.';
-  updateCoordinates(0, 0);
+  querySelector('#output').text = 'Please enter account:';
   querySelector("#enter").onClick.listen(clickedOnEnter);
 }
 
 Environment environment = new Environment("wood");
-
+String account = 'yubao';
+String password = '1+1=?';
+String hints = '1+1=?';
+bool accountPassed = false;
+bool passwordPassed = false;
+int hintsHitLeft = 5;
 clickedOnEnter(e) {
   InputElement input = querySelector("#commands");
   input.style.borderColor = "#55FF55";
   String output = "";
+  if (!accountPassed) {
+    if (input.value != account) {
+      output += "Invalid account. Please reenter: ";
+      var outputHtml = "<div>${output}</div>";
+      querySelector('#output').appendHtml(outputHtml);
+      return;
+    }
+    accountPassed = true;
+    output += "Correct! Please enter the password:";
+    var outputHtml = "<div>${output}</div>";
+    querySelector('#output').appendHtml(outputHtml);
+    return;
+  }
+  if (!passwordPassed) {
+    if (input.value != password) {
+      output += "Incorrect password. Please reenter: ";
+      if (--hintsHitLeft == 0) {
+        output += " (hint: ${hints})";
+      }
+      var outputHtml = "<div>${output}</div>";
+      querySelector('#output').appendHtml(outputHtml);
+      return;
+    }
+    passwordPassed = true;
+    output += "Login successfully. Love u yubao!";
+    var outputHtml = "<div>${output}</div>";
+    querySelector('#output').appendHtml(outputHtml);
+    outputHtml = "<div>Welcome to the game!</div>";
+    querySelector('#output').appendHtml(outputHtml);
+    return;
+  }
+
   switch (input.value) {
     case "left":
-      updateCoordinates(-1, 0);
       output = "You are going left now.";
       break;
     case "right":
-      updateCoordinates(1, 0);
       output = "You are going right now.";
       break;
     case "up":
-      updateCoordinates(0, 1);
       output = "You are going up now.";
       break;
     case "down":
-      updateCoordinates(0, -1);
       output = "You are going down now.";
       break;
     default:
       output = "Please enter one of up, down, left, and right.";
+      var outputHtml = "<div>${output}</div>";
+      querySelector('#output').appendHtml(outputHtml);
+      return;
   }
   var outputHtml = "<div>${output}</div>";
   querySelector('#output').appendHtml(outputHtml);
